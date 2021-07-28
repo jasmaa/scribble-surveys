608c68fdd18e66877ba8050357fd02f5e1d9a945
jasmaa <jasonmaa3955@gmail.com>
2021-07-28 16:25:42 -0400

    Add navbar

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SurveyList from './pages/SurveyList';
 import Survey from './pages/Survey';
@@ -8,15 +9,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Switch>
-          <Route exact path="/surveys" component={SurveyList} />
-          <Route exact path="/surveys/:surveyID" render={props => <Survey key={props.match.params.surveyID} />} />
-          <Route exact path="/export" component={Export} />
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
+    <div>
+      <Navbar />
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/surveys" component={SurveyList} />
+            <Route exact path="/surveys/:surveyID" render={props => <Survey key={props.match.params.surveyID} />} />
+            <Route exact path="/export" component={Export} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ web/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+export default function AppNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar className="px-3" color="light" light expand="md">
+        <NavbarBrand href="/">Scribble Surveys</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/surveys">All</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/create">Create</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/export">Export</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
